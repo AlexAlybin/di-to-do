0898a8a235ebb118ffec1af52aa4ba000fe6eb92
unknown <mr.alexalybin@gmail.com>
2018-12-02 13:01:56 +0200

    Added innerHTML to new cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,60 @@
-let addCardbtn = document.getElementById('add-card-button').addEventListener('click', addNewCard);
+let addCardBtn = document.getElementById('add-card-button');
 
-//Adding new blank card to page
-function addNewCard(){
-//Create new card element
+let deleteCardBtn = document.querySelectorAll('.delete-card-button');
+
+addCardBtn.onclick = function addNewCard(){
+	//Create new card element on page
 	let newCard = document.createElement('div');
 	newCard.className = 'main-card';
+	newCard.innerHTML = '<div class="card-header">' + 
+              				'<input type="text" name="card-title" class="card-title" placeholder="Input card title">' + 
+              				'<button class="delete-card-button"></button>' +
+          				'</div>' +
+          				'<div class="card-items-list">' +
+          				'</div>' +
+          				'<input type="text" name="new-item-name" placeholder="Add to-do" class="card-add-input">'
 
-//Create new card header
-	let newCardHeader = document.createElement('div');
-	newCardHeader.className = 'card-header';
-
-//Create new title and delete button
-	let newCardTitle = document.createElement('input');
-	newCardTitle.setAttribute('type', 'text');
-	newCardTitle.setAttribute('name', 'card-title');
-	newCardTitle.setAttribute('placeholder', 'Card title');
-	newCardTitle.setAttribute('class', 'card-title');
-
-	newCardTitle.addEventListener('submit', setCardTitle);
-
-	let newCardDeleteBtn = document.createElement('button');
-	newCardDeleteBtn.className = 'delete-card-button';
-
-    newCardDeleteBtn.addEventListener('click', deleteCard);
-
-//Add card title and delete button to card header
-	newCardHeader.appendChild(newCardTitle);
-	newCardHeader.appendChild(newCardDeleteBtn);
-
-//Create new list of items
-	let newCardItemsList = document.createElement('div');
-	newCardItemsList.className = 'card-items-list';
-
-//Create new item input field
-	let newCardItemInput = document.createElement('input');
-	newCardItemInput.setAttribute('type', 'text');
-	newCardItemInput.setAttribute('name', 'new-item-name');
-	newCardItemInput.setAttribute('placeholder', 'Add to-do');
-	newCardItemInput.setAttribute('class', 'card-add-input');
-
-	newCardItemInput.addEventListener('keydown', addListItem);
-
-//Add header, items list and item input to card
-	newCard.appendChild(newCardHeader);
-	newCard.appendChild(newCardItemsList);
-	newCard.appendChild(newCardItemInput);
-	
-//Add new card to the page
+    //Add new card to page
 	document.getElementById('cards-area').appendChild(newCard);
 }
 
-//Remove card from page
-function deleteCard(e) {
-	if (confirm('Delete card?')) {
-	e.target.parentNode.parentNode.remove(this.parentNode);
-	}
+for (let i = 0; i < deleteCardBtn.length; i++) {
+	deleteCardBtn[i].addEventListener('click', deleteCard);
+	console.log('Test');		
 }
 
-//Set new card title
-function setCardTitle(e){
-	e.preventDefault();
-	
-	console.log(inputValue);
+function deleteCard(e){
+	console.log(e.target);
 }
+
+// 	let newCardDeleteBtn = document.createElement('button');
+// 	newCardDeleteBtn.className = 'delete-card-button';
+
+//     newCardDeleteBtn.addEventListener('click', deleteCard);
+
+
+
+// 	newCardItemInput.addEventListener('keydown', addListItem);
+
+
+
+//Remove card from page
+// function deleteCard(e) {
+// 	if (confirm('Delete card?')) {
+// 	e.target.parentNode.parentNode.remove(this.parentNode);
+// 	}
+// }
 
 //Add list item
 function addListItem(e){
 	e.preventDefault;
 //Get value from input field
 	let newItemText = e.target.value;
+
+	console.log(e.target.value);
+	
 	if (e.key === 'Enter') {
+
 //Create new card list item
 	let cardItem = document.createElement('div');
 	cardItem.className = 'card-item';
@@ -97,6 +82,7 @@ function addListItem(e){
 	cardItem.appendChild(itemText);
 
 //Add card item to card
-	newCardItemsList.appendChild(cardItem);
+	e.target.previousSibling.appendChild(cardItem);
+	e.target.value = '';
 	}
 }
